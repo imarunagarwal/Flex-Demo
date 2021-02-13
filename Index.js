a45ec498ef8e057054f7bbd3f7b23fb83db651d9
Arun-Agarwal <Arun.Agarwal@coxautoinc.com>
2021-02-13 23:10:41 +0530

    initial commit

---
+++ Index.js
@@ -0,0 +1,16 @@
+botClick = function(){
+    document.getElementById("chatBox").classList.toggle("hide");
+}
+
+sendButtonClick = function(){
+    parent = document.getElementById("messages");
+    message = document.getElementById("inputMessage").value;
+    document.getElementById("inputMessage").value = '';
+    parent.innerHTML += `<div class="leftMessage message">${message}</div><div class="rightMessage message">${message}</div>`;
+    updateScroll();
+}
+
+updateScroll = function() {
+    var element = document.getElementById("chatBox");
+    element.scrollTop = element.scrollHeight;
+  }
